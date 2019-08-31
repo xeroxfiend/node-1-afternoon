cab12a5c7dcef2f38e1c4e52be5b65c08bf8bceb
Brian Lowe <mynameisboy@gmail.com>
2019-08-31 10:24:31 -0600

    finished the api

--- server/getProducts.js
+++ server/getProducts.js
@@ -2,12 +2,11 @@ const products = require("../products.json");
 
 const getProducts = (req, res) => {
   if (req.query.price) {
-    const filtered = products.filter(el => {
-      if (el.price >= parseInt(req.query.price)) {
-          
-        return res.status(200).send(filtered);
-      }
-    });
+    const filtered = products.filter(
+      el => el.price >= parseInt(req.query.price)
+    );
+
+    return res.status(200).send(filtered);
   }
   res.status(200).send(products);
 };
